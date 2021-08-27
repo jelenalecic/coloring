d92fc8fb8e63be84fb4599e83cb3639aaa477d78
jelenalecic <jelena.carnegie@gmail.com>
2021-08-27 12:34:21 +0200

    additions

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,10 @@
+import 'dart:async';
+import 'dart:math';
 import 'dart:typed_data';
-
+import 'package:color_manipulations/generator.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:image/image.dart' as imageLib;
-
-int noOfPixelsPerAxis = 20;
-
-Color abgrToColor(int argbColor) {
-  int r = (argbColor >> 16) & 0xFF;
-  int b = argbColor & 0xFF;
-  int hex = (argbColor & 0xFF00FF00) | (b << 16) | r;
-  return Color(hex);
-}
-
-List<Color> computeColors(Uint8List bytes) {
-  print('computing started');
-  List<Color> colors = [];
-
-  List<int> values = bytes.buffer.asUint8List();
-  imageLib.Image? image = imageLib.decodeImage(values);
-
-  List<int?> pixels = [];
-
-  int? width = image?.width;
-  int? height = image?.height;
-
-  int xChunk = width! ~/ (noOfPixelsPerAxis + 1);
-  int yChunk = height! ~/ (noOfPixelsPerAxis + 1);
-
-  for (int j = 1; j < noOfPixelsPerAxis + 1; j++) {
-    for (int i = 1; i < noOfPixelsPerAxis + 1; i++) {
-      int? pixel = image?.getPixel(xChunk * i, yChunk * j);
-      pixels.add(pixel);
-      colors.add(abgrToColor(pixel!));
-    }
-  }
-
-  print('computing ended');
-  return colors;
-}
 
 void main() {
   runApp(MyApp());
@@ -51,10 +16,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Coloring',
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
-
-      ),
       home: RootWidget(),
     );
   }
@@ -67,30 +28,125 @@ class RootWidget extends StatefulWidget {
   _RootWidgetState createState() => _RootWidgetState();
 }
 
-final String photo =
+final List<String> photos = [
+  photo1,
+  photo2,
+  photo3,
+  photo4,
+  photo5,
+  photo6,
+  photo7,
+  photo8,
+  photo9,
+  photo10,
+  photo11
+];
+
+final String photo1 =
     'https://iaa-network.com/wp-content/uploads/2021/03/Seychelles-arbitration-1.jpg';
+final String photo2 = 'https://i.imgur.com/bmwGs4n.png';
+final String photo3 =
+    'https://media.tacdn.com/media/attractions-splice-spp-674x446/07/6f/f1/aa.jpg';
+final String photo4 =
+    'https://i.pinimg.com/originals/20/0b/95/200b95dfb2efa80d37479764a324b462.jpg';
+
+final String photo5 =
+    'https://assets.rappler.co/612F469A6EA84F6BAE882D2B94A4B421/img/CDCC3B2965FC403F94CD4F3B158F1788/image-2019-01-21-3.jpg';
+final String photo6 = 'https://cdn.wallpapersafari.com/68/60/HgzJbQ.jpg';
+final String photo7 = 'https://wallpaperaccess.com/full/3879268.jpg';
+final String photo8 = 'https://wallpapercave.com/wp/wp2461878.jpg';
+final String photo9 = 'https://wallpapercave.com/wp/gLCTnod.jpg';
+final String photo10 =
+    'https://c4.wallpaperflare.com/wallpaper/827/998/515/ice-cream-4k-in-hd-quality-wallpaper-preview.jpg';
+final String photo11 =
+    'https://img5.goodfon.com/wallpaper/nbig/e/93/tort-malina-shokolad.jpg';
+
+String photo = photo1;
+
+int noOfPaletteColors = 4;
 
 class _RootWidgetState extends State<RootWidget> {
-  List<Color?> colors = [];
+  List<Color> colors = [];
+  List<Color> sortedColors = [];
+  List<Color> palette = [];
+
+  Color primary = Colors.blueGrey;
+  Color primaryText = Colors.black;
+  Color background = Colors.white;
+
+  late Random random;
+  Uint8List? imageBytes;
 
   @override
   void initState() {
     super.initState();
+    random = Random();
     extractColors();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: background,
       key: UniqueKey(),
       appBar: AppBar(
-        title: Text('C o l o r i n g'),
+        backgroundColor: primary,
+        actions: [
+          IconButton(
+              onPressed: () {
+                extractColors();
+              },
+              icon: Icon(Icons.refresh))
+        ],
+        title: Text(
+          'Coloring',
+          style: TextStyle(color: primaryText, letterSpacing: 1),
+        ),
       ),
       body: Container(
-        child: Column(
+        decoration: BoxDecoration(
+            gradient: palette.isEmpty
+                ? null
+                : LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                    stops: [0.01, 0.6, 1],
+                    colors: [
+                      palette.first.withOpacity(0.3),
+                      palette[palette.length ~/ 2],
+                      palette.last.withOpacity(0.9),
+                    ],
+                  )),
+        child: ListView(
           children: [
-            Image.network(photo),
-            _getGrid(),
+            SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              child: imageBytes != null && imageBytes!.length > 0
+                  ? Image.memory(
+                      imageBytes!,
+                      fit: BoxFit.fill,
+                    )
+                  : Center(child: CircularProgressIndicator()),
+              height: 300,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            _getGrids(),
+            Container(
+              color: Colors.white.withOpacity(0.5),
+              padding: EdgeInsets.only(top: 6, bottom: 16),
+              alignment: Alignment.center,
+              child: Column(
+                children: [
+                  Text('Palette of $noOfPaletteColors colors:'),
+                  SizedBox(height: 10),
+                  _getPalette()
+                ],
+              ),
+            )
           ],
         ),
       ),
@@ -98,34 +154,130 @@ class _RootWidgetState extends State<RootWidget> {
   }
 
   Future<void> extractColors() async {
-    Uint8List bytes = (await NetworkAssetBundle(Uri.parse(photo)).load(photo))
+    colors = [];
+    sortedColors = [];
+    palette = [];
+    imageBytes = null;
+
+    setState(() {});
+
+    noOfPaletteColors = random.nextInt(4) + 2;
+    photo = photos[random.nextInt(photos.length)];
+
+    imageBytes = (await NetworkAssetBundle(Uri.parse(photo)).load(photo))
         .buffer
         .asUint8List();
-    print('image loaded');
 
-    colors = await compute(computeColors, bytes);
-    generatePalette();
-
+    colors = await compute(extractPixelsColors, imageBytes);
+    setState(() {});
+    sortedColors = await compute(sortColors, colors);
+    setState(() {});
+    palette = await compute(
+        generatePalette, {keyPalette: colors, keyNoOfItems: noOfPaletteColors});
+    primary = palette.last;
+    primaryText = palette.first;
+    background = palette.first.withOpacity(0.5);
     setState(() {});
   }
 
-  Widget _getGrid() {
-    return colors.isEmpty
-        ? CircularProgressIndicator()
-        : GridView.builder(
-            shrinkWrap: true,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: noOfPixelsPerAxis),
-            itemCount: colors.length,
-            itemBuilder: (BuildContext ctx, index) {
-              return Container(
-                alignment: Alignment.center,
-                child: Container(
-                  color: colors[index],
-                ),
-              );
-            });
+  Widget _getGrids() {
+    return SizedBox(
+      height: 260,
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: colors.isEmpty
+                ? Container(
+                    child: CircularProgressIndicator(),
+                    alignment: Alignment.center,
+                    height: 200,
+                  )
+                : Column(
+                    children: [
+                      Text(
+                        'Extracted Pixels',
+                        style: TextStyle(
+                            color:
+                                palette.isEmpty ? Colors.black : palette.first),
+                      ),
+                      SizedBox(height: 10),
+                      GridView.builder(
+                          shrinkWrap: true,
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: noOfPixelsPerAxis),
+                          itemCount: colors.length,
+                          itemBuilder: (BuildContext ctx, index) {
+                            return Container(
+                              alignment: Alignment.center,
+                              child: Container(
+                                color: colors[index],
+                              ),
+                            );
+                          }),
+                    ],
+                  ),
+          ),
+          SizedBox(width: 10),
+          Expanded(
+            flex: 1,
+            child: sortedColors.isEmpty
+                ? Container(
+                    child: CircularProgressIndicator(),
+                    alignment: Alignment.center,
+                    height: 200,
+                  )
+                : Column(
+                    children: [
+                      Text(
+                        'Sorted Pixels',
+                        style: TextStyle(
+                            color:
+                                palette.isEmpty ? Colors.black : palette.first),
+                      ),
+                      SizedBox(height: 10),
+                      GridView.builder(
+                          shrinkWrap: true,
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: noOfPixelsPerAxis),
+                          itemCount: sortedColors.length,
+                          itemBuilder: (BuildContext ctx, index) {
+                            return Container(
+                              alignment: Alignment.center,
+                              child: Container(
+                                color: sortedColors[index],
+                              ),
+                            );
+                          }),
+                    ],
+                  ),
+          )
+        ],
+      ),
+    );
   }
 
-  void generatePalette() {}
+  Widget _getPalette() {
+    return SizedBox(
+      height: 50,
+      child: palette.isEmpty
+          ? Container(
+              child: CircularProgressIndicator(),
+              alignment: Alignment.center,
+              height: 100,
+            )
+          : ListView.builder(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemCount: palette.length,
+              itemBuilder: (BuildContext context, int index) => Container(
+                color: palette[index],
+                height: 50,
+                width: 50,
+              ),
+            ),
+    );
+  }
 }
